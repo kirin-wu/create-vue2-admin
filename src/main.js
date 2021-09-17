f1a50318fc11a781c964c491cecfea3d43828c24
wkq <1270314454@qq.com>
2021-09-17 09:19:11 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 配置 ElementUI
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+// 配置 全局初始化样式
+import "@/assets/css/reset.scss";
 
 Vue.config.productionTip = false;
 
