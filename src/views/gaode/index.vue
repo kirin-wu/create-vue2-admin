afc0ceaa4f20cf3bfa72afef1561b9e0a456565c
kirinwu <1270314454@qq.com>
2022-06-13 16:32:32 +0800

    styles:地图

--- src/views/gaode/index.vue
+++ src/views/gaode/index.vue
@@ -96,9 +96,9 @@ export default {
               // 设置label标签
               // label默认蓝框白底左上角显示，样式className为：amap-marker-label
               marker.setLabel({
-                offset: new AMap.Pixel(20, 20), //设置文本标注偏移量
-                content: "<div class='info'>南禅寺</div>", //设置文本标注内容
-                direction: "right", //设置文本标注方位
+                offset: new AMap.Pixel(0, 0), //设置文本标注偏移量
+                content: "<div class='info'>无锡市南禅寺景区</div>", //设置文本标注内容
+                direction: "bottom-right", //设置文本标注方位
               });
             });
           });
